f8a4c5dd015edd1a72590c26fd0bc580149b4ae0
ipsita jena <124194586+jena2112@users.noreply.github.com>
2024-05-15 00:45:27 +0530

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: 'LOCALHOST',
+    USER: 'root',
+    PASSWORD:'ipsita93',
+    DB:"ecom_db",
+    dialect : "mysql",
+    POOL: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 1000
+    }
+}
